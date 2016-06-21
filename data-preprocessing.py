c0278c606f1eef13a3734642edade4a96cbbc5bc
Kara Woo <woo.kara@gmail.com>
2016-06-21 13:41:01 -0700

    Add sample usage of function to docstring

--- data-preprocessing.py
+++ data-preprocessing.py
@@ -9,6 +9,18 @@ def getASIN(upc):
     provided with dashes between certain digits). Query the website and
     retrieve ASIN, then wait one second.
 
+    SAMPLE USAGE
+    ------------
+
+    # Look up a single UPC
+    getASIN("876063002233")
+
+    # Loop over multiple UPCs. One of the below returns UPCNOTFOUND, presumably
+    # because this UPC does not have an ASIN because it's not sold by Amazon:
+    upc = ["876063002233", "013000006408", "895296001035", "0-86069-20030-8"]
+    for i in upc:
+        print(getASIN(i))
+
     """
     upc_dig = re.sub("[^0-9]", "", upc)
     url = "http://upctoasin.com/" + upc_dig
