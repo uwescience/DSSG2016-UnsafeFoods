21ada0b1bb6fa75ce49d1f1232f1ef58a6afba4b
Kara Woo <woo.kara@gmail.com>
2016-06-21 13:32:35 -0700

    Strip non-numeric chars. from UPC before lookup

--- data-preprocessing.py
+++ data-preprocessing.py
@@ -1,11 +1,17 @@
 import requests
+import re
 from time import sleep
 
 def getASIN(upc):
-    """Query upctoasin.com for the UPC, return ASIN, and wait one second (per rate
-    limiting instructions)
+    """Query upctoasin.com to determine ASIN from UPC.
+
+    First remove any non-numeric characters from the UPC (UPCs are often 
+    provided with dashes between certain digits). Query the website and
+    retrieve ASIN, then wait one second.
+
     """
-    url = "http://upctoasin.com/" + upc
+    upc_dig = re.sub("[^0-9]", "", upc)
+    url = "http://upctoasin.com/" + upc_dig
     response = requests.get(url)
     sleep(1) # Sleep for one second
     return(response.text)
