245782204243ef5cfdd333ca7376e35d2f5ec83f
Cynthia Vint <cvint13@live.com>
2016-07-05 18:53:04 -0400

    Updated schema to contain recall history table to handle M:N relationship between Products and Recalls

--- sql/CREATE_UNSAFE_FOODS_SCHEMA.sql
+++ sql/CREATE_UNSAFE_FOODS_SCHEMA.sql
@@ -1,40 +1,45 @@
-﻿CREATE TABLE Recalls(
-	recall_id INT PRIMARY KEY NOT NULL,
+﻿CREATE TABLE Recall(
+	recall_id INT PRIMARY KEY,
 	recall_date DATE NOT NULL,
 	reason VARCHAR(100) NOT NULL,
 	company_release_link TEXT NOT NULL,
 	photos_link TEXT NOT NULL
 );
 
-CREATE TABLE Companies(
+CREATE TABLE Company(
 	company_id BIGINT PRIMARY KEY,
 	company_name VARCHAR(100) NOT NULL	
 );
 
-CREATE TABLE Brands(
+CREATE TABLE Brand(
 	brand_id BIGINT PRIMARY KEY,
 	brand_name VARCHAR(100) NOT NULL,
-	company_id BIGINT REFERENCES Companies(company_id)
+	company_id BIGINT REFERENCES Company(company_id)
 );
 
-CREATE TABLE Products(
+CREATE TABLE Product(
 	product_id BIGINT PRIMARY KEY,
-	recall_id INT REFERENCES Recalls(recall_id),
-	brand_id BIGINT REFERENCES Brands(brand_id),
+	brand_id BIGINT REFERENCES Brand(brand_id),
 	asin CHAR(10),
 	upc VARCHAR(12),
 	product_description TEXT
 );
 
-CREATE TABLE Reviewers(
+CREATE TABLE RecallHistory(
+	recall_history_id BIGINT PRIMARY KEY,
+	recall_id INT REFERENCES Recall(recall_id),
+	product_id BIGINT REFERENCES Product(product_id)
+);
+
+CREATE TABLE Reviewer(
 	reviewer_id BIGINT PRIMARY KEY,
 	reviewer_name VARCHAR(30)
 );
 
-CREATE TABLE Reviews(
+CREATE TABLE Review(
 	review_id BIGINT PRIMARY KEY,
-	reviewer_id BIGINT REFERENCES Reviewers(reviewer_id),
-	product_id BIGINT REFERENCES Products(product_id),
+	reviewer_id BIGINT REFERENCES Reviewer(reviewer_id),
+	product_id BIGINT REFERENCES Product(product_id),
 	review_text TEXT,
 	summary VARCHAR(300),
 	overall SMALLINT,
